7d02d3aafff909748668618a2d18d14e12de4126
fan4428 <fan4428@126.com>
2017-11-17 14:21:04 +0800

    Update aes128cbc.go

--- aes128cbc.go
+++ aes128cbc.go
@@ -9,16 +9,23 @@ import (
 )
 
 func main() {
-	text := []byte("fan123fdsajfkldsjak_8")
+	text := []byte("fanpeng0012345678909876543")
 	key := []byte("sfe023f_9fd&fwfl")
 
 	encode, err := Encrypt(text, key)
+	backEncode := encode
 	fmt.Println("encode:", encode)
 	fmt.Println("err:", err)
 
 	uEnc := b64.StdEncoding.EncodeToString([]byte(encode))
 	fmt.Println("uEncerr:", uEnc)
- 
+
+	decode, deerr := Dncrypt(backEncode, key)
+	dzz := string(decode)
+	fmt.Println("decode:", dzz) //字数多了不知道为什么会出现
+	fmt.Println("err:", deerr)
+	r := initRouter()
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 
 }
 
@@ -31,6 +38,23 @@ func Encrypt(plantText, key []byte) ([]byte, error) {
 	plantText = PKCS7Padding(plantText, block.BlockSize())
 
 	blockModel := cipher.NewCBCEncrypter(block, key)
+
+	ciphertext := make([]byte, len(plantText))
+
+	blockModel.CryptBlocks(ciphertext, plantText)
+	return ciphertext, nil
+}
+
+func Dncrypt(plantText, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key) //选择加密算法
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("block:", block)
+	//plantText = PKCS7Padding(plantText, block.BlockSize())
+	blockSize := block.BlockSize()
+
+	blockModel := cipher.NewCBCDecrypter(block, key[:blockSize])
 
 	ciphertext := make([]byte, len(plantText))
 
